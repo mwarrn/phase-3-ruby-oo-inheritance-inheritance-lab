8c5c8c44dc897d35419e7a6edc9d29e890ea5814
Michael Warren <mwarrn@amazon.com>
2022-07-31 17:55:25 -0700

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    # takes in a string and adds that string to the student's @knowledge array
+    def learn(string_element)
+        @knowledge << string_element
+    end
+
+    # returns that student's knowledge array
+    def knowledge
+        @knowledge
+    end
 
 end
